0a8e0a01c42c026a42fa0db61a99673e04f1a9f1
Mohammed Aminu <aminuolawalekan@gmail.com>
2020-11-15 22:12:24 +0100

    initial responsive

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import Layout from "./containers/layout";
 import Home from "./containers/home";
 import "./sass/main.scss";
 import About from "./containers/about";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Aminu Olawale - Software Engineer</title>
+      </Helmet>
       <Layout>
         <Home></Home>
         <About></About>
--- src/containers/about.js
+++ src/containers/about.js
@@ -5,89 +5,89 @@ import { animateScroll } from "react-scroll";
 const About = () => {
   return (
     <div className="about">
-      <div className="about__text">
-        <div
-          className="about__text__header"
-          onClick={() => animateScroll.scrollTo(0)}
-        >
-          <h3>About Me</h3>
-          <div className="about__text__header__rule"></div>
-        </div>
-        <div className="about__text__p">
-          <p>
-            Hello! I am Aminu Mohammed, a Software Engineer based in Lagos,
-            Nigeria.
-          </p>
-          <p>
-            I have a passion for software and spend most of my time building
-            software or tinkering with the ones that pique my interest. I mostly
-            develop and maintain web and mobile applications and while I am not
-            working, I enjoy reading science non-fiction books and some
-            philosophy.
-          </p>
-          <p>
-            I graduated from the University of Lagos, Nigeria, and after working
-            as a motion graphics artist and 3D Animator for a while I joined the
-            dev team at Sendbox where I develop and maintain an e-commerce and
-            logistics platform used by tens of thousands of users daily.
-          </p>
-          <p>
-            Below is the current snapshot of the ever-growing list of tools and
-            technology I work with:
-          </p>
-        </div>
-        <div className="about__text__techs">
-          <ul>
-            <li>
-              <img
-                className="about__text__techs__listIcon"
-                src={chevronRight}
-              ></img>
-              Python
-            </li>
-            <li>
-              <img
-                className="about__text__techs__listIcon"
-                src={chevronRight}
-              ></img>
-              Typescript
-            </li>
-            <li>
-              <img
-                className="about__text__techs__listIcon"
-                src={chevronRight}
-              ></img>
-              Nodejs
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <img
-                className="about__text__techs__listIcon"
-                src={chevronRight}
-              ></img>
-              Javascript
-            </li>
-            <li>
-              <img
-                className="about__text__techs__listIcon"
-                src={chevronRight}
-              ></img>
-              React
-            </li>
-            <li>
-              <img
-                className="about__text__techs__listIcon"
-                src={chevronRight}
-              ></img>
-              Docker
-            </li>
-          </ul>
-        </div>
+      <div className="about__header" onClick={() => animateScroll.scrollTo(0)}>
+        <h3>About Me</h3>
+        <div className="about__header__rule"></div>
       </div>
-      <div className="about__image">
-        <div className="about__image__imageBorder"></div>
-        <div className="about__image__img"></div>
+      <div className="about__content">
+        <div className="about__content__image">
+          <div className="about__content__image__imageBorder"></div>
+          <div className="about__content__image__img"></div>
+        </div>
+        <div className="about__content__text">
+          <div className="about__content__text__p">
+            <p>
+              Hello! I am Aminu Mohammed, a Software Engineer based in Lagos,
+              Nigeria.
+            </p>
+            <p>
+              I have a passion for software and spend most of my time building
+              software or tinkering with the ones that pique my interest. I
+              mostly develop and maintain web and mobile applications and while
+              I am not working, I enjoy reading science non-fiction books and
+              some philosophy.
+            </p>
+            <p>
+              I graduated from the University of Lagos, Nigeria, and after
+              working as a motion graphics artist and 3D Animator for a while I
+              joined the dev team at Sendbox where I develop and maintain an
+              e-commerce and logistics platform used by tens of thousands of
+              users daily.
+            </p>
+            <p>
+              Below is the current snapshot of the ever-growing list of tools
+              and technology I work with:
+            </p>
+          </div>
+          <div className="about__content__text__techs">
+            <ul>
+              <li>
+                <img
+                  className="about__content__text__techs__listIcon"
+                  src={chevronRight}
+                ></img>
+                Python
+              </li>
+              <li>
+                <img
+                  className="about__content__text__techs__listIcon"
+                  src={chevronRight}
+                ></img>
+                Typescript
+              </li>
+              <li>
+                <img
+                  className="about__content__text__techs__listIcon"
+                  src={chevronRight}
+                ></img>
+                Nodejs
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <img
+                  className="about__content__text__techs__listIcon"
+                  src={chevronRight}
+                ></img>
+                Javascript
+              </li>
+              <li>
+                <img
+                  className="about__content__text__techs__listIcon"
+                  src={chevronRight}
+                ></img>
+                React
+              </li>
+              <li>
+                <img
+                  className="about__content__text__techs__listIcon"
+                  src={chevronRight}
+                ></img>
+                Docker
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
